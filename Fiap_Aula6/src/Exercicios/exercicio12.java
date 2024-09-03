e857a3d39f32be29ee9c8645f9dbbf67fde4c0d9
Yan-Wendel <yanwendelrodriguesmachado@gmail.com>
2024-09-03 16:24:02 -0300

    exer

--- Fiap_Aula6/src/Exercicios/exercicio12.java
+++ Fiap_Aula6/src/Exercicios/exercicio12.java
@@ -1,10 +1,12 @@
 package Exercicios;
-
+import java.util.Scanner;
 public class exercicio12 {
-
+/*Faça um programa que leia 3 valores que representam os lados de um triângulo A, B
+e C e ordene-os em ordem decrescente, de modo que o lado A representa o maior
+dos 3 lados. A seguir, determine o tipo de triângulo que estes três lados formam, com
+base nos seguintes casos:*/
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner ler = new Scanner(System.in);
+		
 	}
-
 }
