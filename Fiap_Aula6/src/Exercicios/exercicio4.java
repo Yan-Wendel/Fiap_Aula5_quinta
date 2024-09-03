b3a51e3db5861da4834fec2c32593d1ace47f101
Yan-Wendel <yanwendelrodriguesmachado@gmail.com>
2024-09-03 16:26:53 -0300

    exerc4

--- Fiap_Aula6/src/Exercicios/exercicio4.java
+++ Fiap_Aula6/src/Exercicios/exercicio4.java
@@ -4,7 +4,19 @@ public class exercicio4 {
 
 	public static void main(String[] args) {
 		Scanner ler = new Scanner(System.in);
+		 // Solicita ao usuário os dois valores inteiros
+        System.out.println("Digite o valor de A:");
+        int A = ler.nextInt();
 
+        System.out.println("Digite o valor de B:");
+        int B = ler.nextInt();
+
+        // Verifica se A e B são múltiplos
+        if (A % B == 0 || B % A == 0) {
+            System.out.println("São Múltiplos");
+        } else {
+            System.out.println("Não são Múltiplos");
+        }
 	}
 
 }
